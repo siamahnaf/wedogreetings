69b48e434a23e166556d13179d784882455a2ae5
Siam Ahnaf <siamahnaf198@gmail.com>
2023-08-13 22:46:15 +0600

    Adding fixing

--- src/Helper/CancelTemplate.tsx
+++ src/Helper/CancelTemplate.tsx
@@ -31,7 +31,7 @@ const CancelTemplate = () => {
                     <Img src="https://res.cloudinary.com/dbceuy3oy/image/upload/v1691944012/Logo_bxg349.png" alt="cat" width="281" height="42" className="mx-auto w-[300px]" />
                     <Text className="text-3xl font-bold">Oh! no your payment failed</Text>
                     <Text className="mb-8">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
                     </Text>
                     <Link href="https://staging.wedogreetings.co.uk/" className="bg-[#0AC2FF] text-white text-sm py-2 px-8 rounded-md uppercase font-bold">
                         Continue Shopping
